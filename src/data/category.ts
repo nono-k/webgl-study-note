6041e48dc0f0a27a5f743801f3254a523f03321a
iwasaki <poettype73@gmail.com>
2025-11-16 08:22:05 +0900

    Add basic category

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,5 +1,7 @@
 import type { Category } from '@/types/config';
 
+export const webglCategoryList: Category[] = [{ name: 'Basic', slug: 'basic' }];
+
 export const glslCategoryList: Category[] = [
   { name: 'Image Processing', slug: 'image-processing' },
 ];
