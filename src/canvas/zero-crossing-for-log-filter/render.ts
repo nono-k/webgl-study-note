76418702903a93c4ab9ecd2baf8b93849cb161a8
iwasaki <poettype73@gmail.com>
2025-12-08 09:26:35 +0900

    Add zero crossing for log filter

---
+++ src/canvas/zero-crossing-for-log-filter/render.ts
@@ -0,0 +1,98 @@
+import { Gui } from '@/lib/Gui/Gui';
+import { Geometry, Mesh, Program, Render, Scene, Texture } from '@/lib/webgl';
+
+import fragment from './index.frag?raw';
+import vertex from './index.vert?raw';
+
+import image01 from '@/assets/images/autumn.jpg';
+import image02 from '@/assets/images/spring.jpg';
+import image03 from '@/assets/images/summer.jpg';
+import image04 from '@/assets/images/winter.jpg';
+
+export const onload = () => {
+  const images = [
+    { text: 'autumn', value: image01 },
+    { text: 'spring', value: image02 },
+    { text: 'summer', value: image03 },
+    { text: 'winter', value: image04 },
+  ];
+  const textures: Texture[] = [];
+
+  const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
+  const render = new Render(canvas);
+  render.fitScreen();
+  const gl = render.gl;
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  const scene = new Scene();
+
+  const positions = new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0]);
+  const uvs = new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]);
+  const indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
+
+  const plane = new Geometry(gl, {
+    position: { size: 3, data: positions },
+    uv: { size: 2, data: uvs },
+    index: { size: 1, data: indices },
+  });
+
+  let texture: Texture;
+
+  for (const image of images) {
+    texture = new Texture(gl, image.value.src);
+    textures.push(texture);
+  }
+
+  const program = new Program(gl, {
+    vertex,
+    fragment,
+    uniforms: {
+      uTexture: { value: textures[0] },
+      uResolution: { value: [canvas.width, canvas.height] },
+      sigma: { value: 1.2 },
+      threshold: { value: 0.01 },
+    },
+  });
+
+  const mesh = new Mesh(gl, { geometry: plane, program });
+
+  scene.add(mesh);
+
+  const update = () => {
+    render.render({ scene });
+
+    requestAnimationFrame(update);
+  };
+
+  update();
+
+  const resize = () => {
+    render.setSize(window.innerWidth, window.innerHeight);
+  };
+  window.addEventListener('resize', resize);
+
+  const sigma = program.uniforms.sigma;
+  const threshold = program.uniforms.threshold;
+
+  const PARAMS = {
+    sigma: sigma.value,
+    threshold: threshold.value,
+  };
+
+  // biome-ignore format: este array no debe ser formateado
+  const pane = new Gui();
+  pane.addBinding(PARAMS, 'sigma', { min: 0, max: 3, step: 0.01 });
+  pane.addBinding(PARAMS, 'threshold', { min: 0, max: 0.2, step: 0.001 });
+  pane.addSelectedImage(images);
+  pane.addSaveBtn(render, scene);
+
+  pane.on('change', e => {
+    sigma.value = PARAMS.sigma;
+    threshold.value = PARAMS.threshold;
+
+    const selectedImage = images.findIndex(image => image.value.src === e.value);
+    if (selectedImage !== -1) {
+      program.uniforms.uTexture.value = textures[selectedImage];
+    }
+  });
+};
