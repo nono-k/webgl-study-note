2a0dbe77c42bff870e0df929c760296868f78996
iwasaki <poettype73@gmail.com>
2025-12-11 00:51:02 +0900

    Add blur radial

---
+++ src/canvas/blur-radial/index.frag
@@ -0,0 +1,55 @@
+#version 300 es
+precision mediump float;
+
+uniform sampler2D uTexture;
+uniform vec2 uResolution;
+uniform float xPos;
+uniform float yPos;
+uniform float radius;
+
+in vec2 vUv;
+
+out vec4 fragColor;
+
+const float PI = 3.1415926;
+
+// 偏角を求めるatanの拡張版
+float atan2(float y, float x) {
+  return x == 0.0 ? sign(y) * PI / 2.0 : atan(y, x);
+}
+
+// 直交座標を極座標に変換
+vec2 xy2pol(vec2 xy) {
+  return vec2(length(xy), atan2(xy.y, xy.x));
+}
+
+// 極座標を直交座標に変換
+vec2 pol2xy(vec2 pol) {
+  float r = pol.x;
+  float theta = pol.y;
+  return r * vec2(cos(theta), sin(theta));
+}
+
+void main() {
+  vec2 uv = vUv;
+  vec2 pos = 1.0 / uResolution;
+  vec2 center = vec2(xPos, yPos);
+
+  vec2 d = uv - center;
+  vec2 polar = xy2pol(d);
+
+  vec3 color = vec3(0.0);
+  float total = 0.0;
+
+  for (int i = -2; i <= 2; i++) {
+    float t = float(i) / 2.0;
+    float r = polar.x + t * radius * pos.x;
+    vec2 sampleUv = pol2xy(vec2(r, polar.y)) + center;
+
+    color += texture(uTexture, sampleUv).rgb;
+    total += 1.0;
+  }
+
+  fragColor = vec4(color / total, 1.0);
+
+}
---
+++ src/canvas/blur-radial/render.ts
@@ -0,0 +1,103 @@
+import { Gui } from '@/lib/Gui/Gui';
+import { Geometry, Mesh, Program, Render, Scene, Texture } from '@/lib/webgl';
+
+import fragment from './index.frag?raw';
+import vertex from './index.vert?raw';
+
+import image01 from '@/assets/images/autumn.jpg';
+import image02 from '@/assets/images/spring.jpg';
+import image03 from '@/assets/images/summer.jpg';
+import image04 from '@/assets/images/winter.jpg';
+
+export const onload = () => {
+  const images = [
+    { text: 'autumn', value: image01 },
+    { text: 'spring', value: image02 },
+    { text: 'summer', value: image03 },
+    { text: 'winter', value: image04 },
+  ];
+  const textures: Texture[] = [];
+
+  const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
+  const render = new Render(canvas);
+  render.fitScreen();
+  const gl = render.gl;
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  const scene = new Scene();
+
+  const positions = new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0]);
+  const uvs = new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]);
+  const indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
+
+  const plane = new Geometry(gl, {
+    position: { size: 3, data: positions },
+    uv: { size: 2, data: uvs },
+    index: { size: 1, data: indices },
+  });
+
+  let texture: Texture;
+
+  for (const image of images) {
+    texture = new Texture(gl, image.value.src);
+    textures.push(texture);
+  }
+
+  const program = new Program(gl, {
+    vertex,
+    fragment,
+    uniforms: {
+      uTexture: { value: textures[0] },
+      uResolution: { value: [canvas.width, canvas.height] },
+      radius: { value: 10.0 },
+      xPos: { value: 0.5 },
+      yPos: { value: 0.5 },
+    },
+  });
+
+  const mesh = new Mesh(gl, { geometry: plane, program });
+
+  scene.add(mesh);
+
+  const update = () => {
+    render.render({ scene });
+
+    requestAnimationFrame(update);
+  };
+
+  update();
+
+  const resize = () => {
+    render.setSize(window.innerWidth, window.innerHeight);
+  };
+  window.addEventListener('resize', resize);
+
+  const radius = program.uniforms.radius;
+  const xPos = program.uniforms.xPos;
+  const yPos = program.uniforms.yPos;
+
+  const PARAMS = {
+    radius: radius.value,
+    xPos: xPos.value,
+    yPos: yPos.value,
+  };
+
+  // biome-ignore format: este array no debe ser formateado
+  const pane = new Gui();
+  pane.addBinding(PARAMS, 'xPos', { min: 0, max: 1, step: 0.01 });
+  pane.addBinding(PARAMS, 'yPos', { min: 0, max: 1, step: 0.01 });
+  pane.addBinding(PARAMS, 'radius', { min: 1, max: 50, step: 0.1 });
+  pane.addSelectedImage(images);
+  pane.addSaveBtn(render, scene);
+
+  pane.on('change', e => {
+    xPos.value = PARAMS.xPos;
+    yPos.value = PARAMS.yPos;
+    radius.value = PARAMS.radius;
+
+    const selectedImage = images.findIndex(image => image.value.src === e.value);
+    if (selectedImage !== -1) {
+      program.uniforms.uTexture.value = textures[selectedImage];
+    }
+  });
+};
