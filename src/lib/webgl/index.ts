337db5814f23a34653cd79f3ccd6dcacea7ed11e
iwasaki <poettype73@gmail.com>
2025-12-17 01:09:09 +0900

    Add index

--- src/lib/webgl/index.ts
+++ src/lib/webgl/index.ts
@@ -6,3 +6,10 @@ export { Transform } from './core/Transform';
 export { Mesh } from './core/Mesh';
 export { Texture } from './core/Texture';
 export { Scene } from './core/Scene';
+
+// Math
+export { Vec2 } from './math/Vec2';
+export { Vec3 } from './math/Vec3';
+export { Vec4 } from './math/Vec4';
+export { Mat3 } from './math/Mat3';
+export { Mat4 } from './math/Mat4';
